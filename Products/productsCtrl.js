928ba193f53bb33c9e0151ed8670ba65cb530d06
masireddy1984@gmail.com <masireddy1984@gmail.com>
2016-08-12 21:25:41 +0530

    Added JSON data with promises and filters

--- Products/productsCtrl.js
+++ Products/productsCtrl.js
@@ -1,6 +1,16 @@
 angular.module("products")
     .controller("productsCtrl",["$scope","productSvc",function($scope,productSvc){
-    $scope.products = productSvc.getProducts();
+   // $scope.products = productSvc.getProducts()
+     productSvc.getProducts()   
+          .then(function(response){
+            $scope.products = response ;
+    })
+          .catch(function(response){
+            $scope.error = response ;
+     })
+          .finally(function(response){
+            console.log("I am executed finally");
+     })
     
     $scope.addItemsToCart = function(item) {
        productSvc.myCart(item);
